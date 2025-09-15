0cf01f7d3c727bd52ab77c9ca0e6ac4f982ba912
Law Abiding Coder <colton.trash5.5@proton.me>
2025-09-15 12:17:35 -0500

    Delete AIOHHF/AIOHHF/obj directory

--- AIOHHF/AIOHHF/obj/Debug/net472/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace AIOHHF
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "AIOHHF";
-        public const string PLUGIN_NAME = "AIOHHF";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
