04f10eb93fb226e4978f1f58f815af324300fa88
Law Abiding Coder <colton.trash5.5@proton.me>
2025-10-30 10:41:38 -0500

    attempted to add feature of autoclosing

--- AIOHHF/AIOHHF/Mono/HandHeldPlayerTool.cs
+++ AIOHHF/AIOHHF/Mono/HandHeldPlayerTool.cs
@@ -1,4 +1,5 @@
 using AIOHHF.Items.Equipment;
+using UnityEngine;
 
 
 namespace AIOHHF.Mono;
@@ -9,6 +10,7 @@ public class HandHeldPlayerTool : PlayerTool
     public PowerRelay relay;
     public HandHeldBatterySource battery;
     public StorageContainer storageContainer;
+    private double _counter = 0;
     public override void Awake()
     {
         fab = gameObject.GetComponent<HandHeldFabricator>();
@@ -22,6 +24,8 @@ public class HandHeldPlayerTool : PlayerTool
     public override bool OnRightHandDown()
     {
         fab.opened = true;
+        fab.animator.SetBool(AnimatorHashID.open_fabricator, true);
+        _counter = 0f;
         uGUI.main.craftingMenu.Open(Plugin.Aiohhf.TreeType, fab);
         return true;
     }
@@ -40,6 +44,23 @@ public class HandHeldPlayerTool : PlayerTool
     public void Update()
     {
         gameObject.transform.localScale = Plugin.Aiohhf.PostScaleValue;
+        _counter += Time.deltaTime;
+        if (_counter >= 7f
+            && !uGUI.main.craftingMenu.isActiveAndEnabled)
+        {
+            fab.animator.SetBool(AnimatorHashID.open_fabricator, false);
+            _counter = 0;
+        }
+        else if (uGUI.main.craftingMenu.isActiveAndEnabled)
+        {
+            fab.animator.SetBool(AnimatorHashID.open_fabricator, true);
+            _counter = 0;
+        }
+
+        if (fab.crafterLogic.inProgress && !fab.animator.GetBool(AnimatorHashID.open_fabricator))
+        {
+            fab.animator.SetBool(AnimatorHashID.open_fabricator, true);
+        }
     }
 
     public override void OnDraw(Player p)
@@ -47,6 +68,14 @@ public class HandHeldPlayerTool : PlayerTool
         base.OnDraw(p);
         if (fab.animator == null) return;
         fab.animator.SetBool(AnimatorHashID.open_fabricator, true);
+        _counter = 0;
     }
-    
+
+    public override void OnHolster()
+    {
+        base.OnHolster();
+        if (fab.animator == null) return;
+        fab.animator.SetBool(AnimatorHashID.open_fabricator, false);
+        _counter = 0;
+    }
 }
